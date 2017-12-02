474a520889d9ae73f430325e0e599170d8addb50
Niels Boehm <blubberdiblub@gmail.com>
2017-12-02 10:48:34 +0100

    Add test context.

---
+++ tests/context.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""Provide explicit import context for tests."""
+
+import os.path
+import sys
+sys.path.insert(0, os.path.abspath('..'))
+
+import asyncio_cmdline
